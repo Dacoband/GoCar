e97ca83ccde4a8105fea448e7f87f5bd1045c132
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-03-12 13:50:26 +0700

    first commit

---
+++ models/car.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Car struct {
+	CarID     uuid.UUID `json: "car_id"`
+	Name      string    `json: "name"`
+	Year      string    `json: "year"`
+	Brand     string    `json: "brand"`
+	FuelType  string    `json: "fuel_type"`
+	Engine    Engine    `json: "engine"`
+	Price     float64   `json: "price"`
+	CreatedAt time.Time `json"created_at"`
+	UpdatedAt time.Time `json"updated_at"`
+}
+
+type CarRequest struct {
+	Name     string  `json: "name"`
+	Year     string  `json: "year"`
+	Brand    string  `json: "brand"`
+	FuelType string  `json: "fuel_type"`
+	Engine   Engine  `json: "engine"`
+	Price    float64 `json: "price"`
+}
